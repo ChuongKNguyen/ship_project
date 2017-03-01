04ff3e2588e1f169301f1357d04cef31de4fabb7
Chuong Nguyen <tinchuong@yahoo.fr>
2017-03-01 16:18:26 -0500

    styling with sass

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
-  get 'home/index'
+  root 'home#index'
   resources :users, :boats, :jobs
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
