b06252362bc17a223bba1d378216c332402ce325
Chuong Nguyen <tinchuong@yahoo.fr>
2017-03-02 15:13:57 -0500

    jobs function created

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,31 @@
 class JobsController < ApplicationController
+	def index
+		@jobs=Job.all
+	end	
+
+	def new 
+		@job= Job.new
+		@boats = Boat.all
+	end 
+
+	def create 
+		@job= Job.new(job_params)
+
+  		if @job.save 
+			redirect_to jobs_path
+		else 
+			render 'new'
+		end
+	end
+
+	def show 
+		@job = Job.find(params[:id])
+	end  
+
+	private
+
+	def job_params
+		params.require(:job).permit(:status_job, :container, :cargo, :origin, :destination, :job_name, :cost, :job_description, :user_id, :boat_id)
+	end
+
 end
