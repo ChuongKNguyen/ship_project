5a0b35babd194e9dbcad2778c688ae2563d39ba3
Chuong Nguyen <tinchuong@yahoo.fr>
2017-03-06 14:38:52 -0500

    join table BoatJob

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,7 @@
 class JobsController < ApplicationController
 	def index
 		@jobs=Job.all.order('created_at DESC')
+		@boatjob = BoatJob.all
 	end	
 
 	def new 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   root 'home#index'
-  resources :users, :boats, :jobs
+  resources :users, :boats, :jobs, :boatsjobs
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
